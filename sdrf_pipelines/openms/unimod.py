26707d5ac0d1ca3723274bb23a43e47a708ffdb0
yperez <ypriverol@gmail.com>
2020-07-09 22:27:10 +0100

    0.0.8

--- sdrf_pipelines/openms/unimod.py
+++ sdrf_pipelines/openms/unimod.py
@@ -1,7 +1,7 @@
 import pathlib
 import re
 import xml.etree.ElementTree as et
-
+import pkg_resources
 
 class PTMSite(object):
 
@@ -45,7 +45,7 @@ class UnimodDatabase:
   hidden = True
 
   def __init__(self, **kwargs):
-    self.unimodfile = str(pathlib.Path(__file__).parent) + "/unimod.xml"
+    self.unimodfile = pkg_resources.resource_filename(__name__, "unimod.xml")
     self.hidden = kwargs.get("hidden", True)
     node = et.parse(self.unimodfile)
     root = node.getroot()
