5f7d155242c37fa88340cff95758387a237cead9
ypriverol <ypriverol@gmail.com>
2021-01-29 12:30:55 +0000

    technology type

--- sdrf_pipelines/sdrf/sdrf_schema.py
+++ sdrf_pipelines/sdrf/sdrf_schema.py
@@ -314,7 +314,6 @@ cell_lines_schema = SDRFSchema([
              optional_type=False)
 ], min_columns=7)
 
-
 mass_spectrometry_schema = SDRFSchema([
     SDRFColumn('assay name', [LeadingWhitespaceValidation(), TrailingWhitespaceValidation()],
              allow_empty=True,
