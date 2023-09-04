f5c91d6c77b7343c139d649fac8d9f455dbf2770
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-09-04 16:00:12 +0100

    changes in unimod parser

--- sdrf_pipelines/openms/unimod.py
+++ sdrf_pipelines/openms/unimod.py
@@ -1,5 +1,5 @@
 import re
-import xml.etree.ElementTree as et
+import defusedxml.ElementTree as et
 
 import pkg_resources
 
@@ -46,6 +46,9 @@ class PostTranslationalModification:
 
     def get_delta_composition(self):
         return self._delta_composition
+
+    def to_str(self):
+        return f"{self.get_accession()} {self.get_name()} {self.get_delta_mono_mass()} {self.get_delta_composition()}"
 
 
 class UnimodDatabase:
@@ -120,44 +123,6 @@ class UnimodDatabase:
             mod = PostTranslationalModification(ontology_term, ma["delta_composition"], sites, ma["delta_mono_mass"])
             self.modifications.append(mod)
 
-    def get_label(self, label):
-        mod = self.modifications.get(label, None)
-        return mod
-
-    def get_element(self, name):
-        el = self.elements.get(name, None)
-        return el
-
-    def list_labels(self, search):
-        labels = []
-        lre = re.compile(search)
-        for k in self.modifications.keys():
-            l = lre.search(k)
-            if l is not None:
-                labels.append(k)
-        return labels
-
-    def get_neutral_loss(self, label, site):
-        mod = self.modifications.get(label, None)
-        if mod is not None:
-            try:
-                nl = []
-                for n in mod["sites"][site]["NeutralLoss"]:
-                    if n["composition"] != "0":
-                        nl.append(n)
-                return nl
-            except:
-                return []
-        return []
-
-    def get_delta_mono(self, label):
-        mod = self.modifications.get(label, None)
-        if mod is not None:
-            try:
-                val = float(mod["delta_mono_mass"])
-                return val
-            except:
-                pass
 
     def get_by_accession(self, accession):
         for mod in self.modifications:
---
+++ sdrf_pipelines/tests/test_unimod.py
@@ -0,0 +1,19 @@
+from sdrf_pipelines.openms.unimod import UnimodDatabase
+
+
+def test_search_mods_by_keyword():
+    unimod = UnimodDatabase()
+    ptm = unimod.get_by_accession("UNIMOD:21")
+    print(ptm.get_name())
+
+def test_search_mods_by_keyword():
+    unimod = UnimodDatabase()
+    ptms = unimod.search_mods_by_keyword("Phospho")
+    for ptm in ptms:
+        print(ptm.to_str())
+
+if __name__ == "__main__":
+    test_search_mods_by_keyword()
+
+
+
