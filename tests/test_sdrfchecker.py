a9224d6b981399f9c9ca65e10f92d3b997803904
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-07-28 08:05:29 +0100

    tests fixed

--- tests/test_sdrfchecker.py
+++ tests/test_sdrfchecker.py
@@ -29,6 +29,18 @@ def test_validate_srdf_fails_on_bad_file2(shared_datadir, on_tmpdir):
     expected_error = "The following columns are mandatory and not present in the SDRF: characteristics[biological replicate] -- ERROR"
     assert expected_error in result.output, result.output
 
+def test_validate_srdf_fails_on_bad_file3(shared_datadir, on_tmpdir):
+    """
+    :return:
+    """
+    test_sdrf = shared_datadir / "erroneous/example.sdrf.tsv"
+    result = run_and_check_status_code(cli, ["validate-sdrf", "--sdrf_file", str(test_sdrf)], 1)
+
+    expected_errors = ["Make sure your SDRF have a sample characteristics or data comment 'concentration of' for your factor value column 'factor value[concentration of]' -- ERROR",
+                       "Factor 'factor value[compound]' and column 'characteristics[compound]' do not have the same values for the following rows: [11, 20] -- ERROR"]
+    for expected_error in expected_errors:
+        assert expected_error in result.output, result.output
+
 
 reference_samples = [
     "reference/PXD002137/PXD002137.sdrf.tsv",
