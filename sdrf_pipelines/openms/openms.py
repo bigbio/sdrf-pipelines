87e051fb470c1ac30d81cc20e48174ca5f7b9a33
fabianegli <fabian.egli@protonmail.ch>
2025-05-05 14:43:56 +0200

    supply useful data in error message

--- sdrf_pipelines/openms/openms.py
+++ sdrf_pipelines/openms/openms.py
@@ -1137,7 +1137,7 @@ class OpenMS:
             else:
                 raise Exception(
                     "Failed to find any supported labels. Supported labels are 'silac', 'label free "
-                    "sample', 'ITRAQ', and tmt labels in the format 'TMT131C'"
+                    f"sample', 'ITRAQ', and tmt labels in the format 'TMT131C', found {labels}"
                 )
 
             # Why is the file name modified on the experimental design but not in the openms.tsv?
