5253e8c217b12a7c62f8a3b5774608b79e342ed6
fabianegli <fabian.egli@protonmail.ch>
2025-05-19 16:35:33 +0200

    remove print statement from test

--- tests/test_ontology.py
+++ tests/test_ontology.py
@@ -4,7 +4,6 @@ from sdrf_pipelines.ols.ols import OlsClient
 def test_ontology():
     ols = OlsClient()
     ontology_list = ols.ols_search("homo sapiens", ontology="ncbitaxon")
-    print(ontology_list)
     assert len(ontology_list) > 0
 
 
